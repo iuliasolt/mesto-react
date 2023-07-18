e7d4f4e3ff6e92e0c98e946d7282be8d2b8ce188
Julia Soltyk <iulia1502@yandex.ru>
2023-07-18 10:41:15 +0300

    Исправлена степень затемнения слоя оверлей

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,6 +1,6 @@
 function ImagePopup(props) {
     return (
-      <div className={`popup popup_type_image" ${props.card ? `popup_opened` : ''} `} onClick={props.onCloseClick}>
+      <div className={`popup popup_type_image ${props.card ? `popup_opened` : ''} `} onClick={props.onCloseClick}>
                 <div className="popup__card-container">
                 <button className="popup__close popup__close_image" type="button" onClick={props.onClose} aria-label="Кнопка закрыть"></button>
                     <img className="popup__image" src={props.card ? props.card.link : ''} alt={props.card ? props.card.name : ''} />
